a822a4ecfb6a2beaa3580769777d0e35f0c066b6
Jorge Albericio <jorge.albericio@gmail.com>
2012-04-25 12:38:46 +0200

    Addition of the L2_NRSS_IS state

--- ruby/generated/MSI_MOSI_inclusive-RnR/protocol_name.h
+++
@@ -1,7 +0,0 @@
-// Auto generated C++ code started by main.C:135
-
-#ifndef PROTOCOL_NAME_H
-#define PROTOCOL_NAME_H
-
-const char CURRENT_PROTOCOL[] = "MSI_MOSI_inclusive-RnR";
-#endif // PROTOCOL_NAME_H
