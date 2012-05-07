a39b76e5e0e3a2cc392bd419bcf6ddf622418950
Jorge Albericio <jorge.albericio@gmail.com>
2012-05-07 16:24:38 +0200

    clean 2

--- slicc/amd64-linux/obj/fileio.d
+++
@@ -1,5 +0,0 @@
-amd64-linux/obj/fileio.d amd64-linux/obj/fileio.o: generator/fileio.C generator/fileio.h slicc_global.h \
-  /home/jalbe/comun/gems/common/std-includes.h \
-  /home/jalbe/comun/gems/common/Map.h \
-  /home/jalbe/comun/gems/common/Vector.h \
-  /home/jalbe/comun/gems/common/std-includes.h
--- slicc/amd64-linux/obj/html_gen.d
+++
@@ -1,9 +0,0 @@
-amd64-linux/obj/html_gen.d amd64-linux/obj/html_gen.o: generator/html_gen.C generator/html_gen.h slicc_global.h \
-  /home/jalbe/comun/gems/common/std-includes.h \
-  /home/jalbe/comun/gems/common/Map.h \
-  /home/jalbe/comun/gems/common/Vector.h \
-  /home/jalbe/comun/gems/common/std-includes.h symbols/StateMachine.h \
-  slicc_global.h /home/jalbe/comun/gems/common/Vector.h \
-  /home/jalbe/comun/gems/common/Map.h symbols/Symbol.h ast/Location.h \
-  slicc_global.h generator/fileio.h symbols/SymbolTable.h symbols/Type.h \
-  symbols/Var.h symbols/Func.h symbols/StateMachine.h
